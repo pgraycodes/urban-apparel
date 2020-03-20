df954277542ed4dfe913fe6dbeb8ae3fc5507be3
unknown <paulgray0813@gmail.com>
2020-03-19 17:16:16 -0700

    made canges to add stripe functionality

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,8 @@ import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { createStructuredSelector } from "reselect";
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import {selectCurrentUser} from "../../redux/user/user.selectors";
-import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { ReactComponent as Logo } from "../../assets/magnet.svg";
+
 
 import "./header.styles.scss";
 
@@ -16,6 +17,7 @@ const Header = ({ currentUser, hidden }) => (
     <Link className="logo-container" to="/">
       <Logo className="logo" />
     </Link>
+    <h2 className= 'name'>RBAN APARREL</h2>
     <div className="options">
       <Link className="option" to="/shop">
         SHOP
